de19f459ddd581f4eb2cf3e30e619d2bd9c15bd9
ntut-xuan <t109590031@ntut.org.tw>
2024-03-08 18:43:56 +0800

    Implement: Footer

--- src/footer.js
+++ src/footer.js
@@ -2,7 +2,7 @@ function Footer(){
     return (
         <div className="container-fluid" style={{width: "100%", backgroundColor: "#777777", padding: '1rem', display: "flex", flexDirection: "column", gap: '1rem'}}>
             <p className="text-white text-center mb-0"> 地址：10608台北市大安區忠孝東路三段 1 號 | 電話：02-27712171 </p>
-            <p className="text-white text-center mb-0"> 信箱：cisc2024@ccisa.org.tw Copyright © 2024 中華民國資訊安全學會 </p>
+            <p className="text-white text-center mb-0"> 信箱：cisc2024.ntut@gmail.com Copyright © Taipei Tech </p>
         </div>
     )
 }
